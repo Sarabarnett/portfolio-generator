b70da56764aabd143db16bcd6ad45936687f3daf
SBarn11 <sarabarnett1@outlook.com>
2021-12-27 14:34:48 -0700

    create HTML file and clean up code

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 
+// const printProfileData = profileDataArr => {
+//   for (let i=0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//   console.log('============');
 
-const printProfileData = profileDataArr => {
-  for (let i=0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('============');
-
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
   
-};
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if(err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+})
